63995defc015dcee44b7fdc1cface2c26ba70b92
FaridRey <89561572+faridfr45@users.noreply.github.com>
2022-06-02 16:49:33 +0700

    simple login and register finish

---
+++ app/src/main/java/com/bangkit/getguide/onboarding/screens/ThirdScreen.kt
@@ -0,0 +1,42 @@
+package com.bangkit.getguide.onboarding.screens
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.bangkit.getguide.R
+import com.bangkit.getguide.authentication.LoginActivity
+
+class ThirdScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_third_screen, container, false)
+
+        val finishButton = view.findViewById<Button>(R.id.nextButton)
+
+        finishButton.setOnClickListener {
+//            onBoardingFinished()
+
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        return view
+    }
+
+    private fun onBoardingFinished(){
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
+    }
+}
